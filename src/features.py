4090b45bb47a0b2c50794a76859985f41fe36603
Elizabeth Garza <garza.elizabeth9@gmail.com>
2020-07-28 16:17:27 -0400

    edited features.py

--- src/features.py
+++ src/features.py
@@ -3,10 +3,8 @@ Extracts suffix and word context features.
 """
 
 
-import typing
 
-
-def suff_features(token: str) -> List: 
+def suff_features(token):
     
     if len(token)>3: 
         suff_features = [f"suf1={token[-1]}", 
@@ -28,7 +26,7 @@ def suff_features(token: str) -> List:
         return suff_features
 
 
-def extract_features(tokenized_sent:  List) ->  List
+def extract_features(tokenized_sent):  
     
     features = []
     
@@ -36,33 +34,33 @@ def extract_features(tokenized_sent:  List) ->  List
         if index==0:
             features.append([f"t[0]={token}", 
                              "__BOS__"]+suff_features(token))
-        if index==1:
-            features.append([f"t[0]={token}", 
+        elif index==1:
+            if len(tokenized_sent)==2: 
+                features.append([f"t[0]={token}", 
+                            f"t[-1]={tokenized_sent[index-1]}"]+suff_features(token))
+            else: 
+                features.append([f"t[0]={token}", 
                              f"t[-1]={tokenized_sent[index-1]}", 
                              f"t[+1]={tokenized_sent[index+1]}", 
                              f"t[-1]={tokenized_sent[index-1]}^t[+1]={tokenized_sent[index+1]}"]+suff_features(token))
-        if index>1 and index<(len(tokenized_sent)-3):
-            if len(token)>3:
-                features.append([f"t[0]={token}", 
+        elif index>1 and index<(len(tokenized_sent)-3): 
+                 features.append([f"t[0]={token}", 
                               f"t[-1]={tokenized_sent[index-1]}", 
                               f"t[+1]={tokenized_sent[index+1]}", 
                               f"t[-1]={tokenized_sent[index-1]}^t[+1]={tokenized_sent[index+1]}",
-                              f"t[-2]={tokenized_sent[index-2]}", 
-                              f"t[+2]={tokenized_sent[index+2]}"]+suff_features(token))
-        if index == len(tokenized_sent)-3:
-            if len(token)>3:
-                features.append([f"t[0]={token}", 
+                              f"t[-2]={tokenized_sent[index-2]}"]+suff_features(token))
+        elif index == len(tokenized_sent)-3:
+            features.append([f"t[0]={token}", 
                               f"t[-1]={tokenized_sent[index-1]}", 
                               f"t[+1]={tokenized_sent[index+1]}", 
                               f"t[-1]={tokenized_sent[index-1]}^t[+1]={tokenized_sent[index+1]}",
                               f"t[-2]={tokenized_sent[index-2]}"]  
                               +suff_features(token))
-        if index == len(tokenized_sent)-2:
-            if len(token)>3:
-                features.append([f"t[0]={token}", 
+        elif index == len(tokenized_sent)-2:
+            features.append([f"t[0]={token}", 
                               f"t[-1]={tokenized_sent[index-1]}",
                               f"t[-2]={tokenized_sent[index-2]}"]+suff_features(token))
-        if index == len(tokenized_sent)-1:
+        elif index == len(tokenized_sent)-1:
             features.append([f"t[0]={token}",
                                  "__EOS__", 
                                   f"t[-1]={tokenized_sent[index-1]}",
