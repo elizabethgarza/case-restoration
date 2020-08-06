be5c65d34a5474e9c7a923583dced4fbd58edbb1
Elizabeth Garza <garza.elizabeth9@gmail.com>
2020-08-05 22:20:55 -0400

    added comments to evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -18,7 +18,6 @@ def main(args):
         all_pred_tokens = []
         all_gold_tokens = []
         all_folded_tokens = []
-
         for (gold_line, pred_line) in zip(gold, pred):
             gold_tokens = gold_line.split()
             pred_tokens = pred_line.split()
@@ -27,7 +26,8 @@ def main(args):
                 all_pred_tokens.append(pred_token)
                 all_gold_tokens.append(gold_token)
                 all_folded_tokens.append(gold_token.casefold())
-        
+
+        # computes TOKEN ACCURACY
         assert len(all_pred_tokens) == len(all_gold_tokens) == len(all_folded_tokens)
         TOTAL = len(all_pred_tokens)
         for gold_token, pred_token in zip(all_gold_tokens, all_pred_tokens): 
@@ -37,6 +37,7 @@ def main(args):
                 print(gold_token, pred_token)
                 INCORRECT += 1
 
+        # computes BASELINE ACCURACY
         BASELINE_CORR = 0
         BASELINE_INCORR = 0 
         for gold_token, folded_token in zip(all_gold_tokens, all_folded_tokens): 
@@ -46,29 +47,30 @@ def main(args):
                 BASELINE_INCORR += 1
         assert (BASELINE_CORR + BASELINE_INCORR) == (len(all_pred_tokens))
 
-        FP = 0 
+        # computes PRECISION and RECALL
+        FP = 0 # 'FP' stands for 'false positive', etc.
         FN = 0
         TP = 0 
-        all_cased_tokens = set()
+        all_cased_tokens = set() # these sets are being created in order to carry out an assertion statement below 
         false_pos = set()
         false_neg = set()
         true_pos = set()
         for gold_token, pred_token in zip(all_gold_tokens, all_pred_tokens): 
-            if gold_token.islower():
-                if gold_token != pred_token: 
+            if gold_token == gold_token.casefold(): # e.g. 'and' == 'and' 
+                if gold_token != pred_token: # e.g. 'and' != 'And'
                     all_cased_tokens.add(gold_token.casefold())
-                    false_pos.add(pred_token.casefold())
+                    false_pos.add(pred_token.casefold()) 
                     FP += 1
-                elif gold_token == pred_token: 
-                    true_pos.add(gold_token.casefold())
+                elif gold_token == pred_token: # e.g. 'and' == 'and' 
+                    true_pos.add(gold_token.casefold()) 
                     all_cased_tokens.add(gold_token.casefold())
                     TP += 1
-            elif gold_token != gold_token.islower(): 
-                if gold_token != pred_token: 
+            elif gold_token != gold_token.casefold(): # e.g. 'Mr.' != 'mr.'; or 'UNICEF' != 'Unicef' (An 'UPPER' that is mistagged as a 'TITLE' will be counted as a false negative.)
+                if gold_token != pred_token: # e.g. 'Mr.' != 'mr.'
                     all_cased_tokens.add(gold_token.casefold())
                     false_neg.add(pred_token.casefold())
                     FN += 1
-                elif gold_token == pred_token: 
+                elif gold_token == pred_token: # e.g. 'Mr.' == 'Mr.'
                     true_pos.add(gold_token.casefold())
                     all_cased_tokens.add(gold_token.casefold())
                     TP += 1
@@ -95,8 +97,8 @@ def main(args):
     print(f'TOTAL: {TOTAL}')
     print(f'CORRECT: {CORRECT}')
     print(f'INCORRECT: {INCORRECT}')
+    print(f'PRECISION: {TP / (TP + FP):.4f}') 
     print(f'RECALL: {TP / (TP + FN): .4f}')
-    print(f'PRECISION: {TP / (TP + FP):.4f}')  
     print(f'TOKEN ACCURACY: {CORRECT/TOTAL:.4f}') 
     print('============================================================================================================')  
   
